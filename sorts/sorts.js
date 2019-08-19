ea10085e81da14acb67eb38953cde7ae4a79ffeb
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-08-19 10:13:34 -0600

    figured out merge with for loop. super complicated. but it works. tentitively

--- sorts/sorts.js
+++ sorts/sorts.js
@@ -87,14 +87,64 @@ const list = [5, 7, 3, 4, 10, 20, 100, -50];
 console.log(insertionSort(list));
 
 
-//[1,2,3,4,5] [1,2,3,6,6,7]*
+//[1,2,3,4,4,4,5] [1,2,3,6,6,7]*
 //[1,1*,2,2*,3,3*,4,5,6*,6*,7*]
 
+
+function merge2point0(array, array2) {
+  let focus = [];
+  let multiSet = [];
+  let small = [];
+  let j = 0;
+  if (array.length >= array2.length) {
+    focus = array;
+    small = array2;
+  } else {
+    focus = array2;
+    small = array;
+  }
+
+  for (let i = 0; i < focus.length; i++){
+    if (i > small.length) {
+      multiSet.push(focus[i]);
+    } else if (focus[i] < small[i]) {
+      multiSet.push(focus[i]);
+    }else if (focus[i] > small[i]) {
+      multiSet.push(small[i]);
+    } else if (focus[i] == small[i]) {
+      multiSet.push(focus[i]);
+      multiSet.push(small[i]);
+    }
+    j = i;
+    while (Math.abs(focus[j + 1] - focus[j])<= Math.abs(focus[j]-small[j])) {
+      multiSet.push(focus[j])
+      console.log("focus here is focus", focus[j]);
+      console.log("focus here is j", j);
+      console.log("here is multiset", multiSet);
+      console.log("here is i", i);
+
+      j++
+    }
+    while (Math.abs(small[j + 1] - small[j])<= Math.abs(small[j] - focus[j])) {
+      multiSet.push(small[j])
+      console.log("small here is j", j);
+      console.log("here is i", i);
+      console.log("here is multiset", multiSet);
+
+      j++
+    }
+  }
+  return multiSet;
+}
 function merge(array, array2) {
   let multiSet = [];
   let i = 0;
   let j = 0; 
-  while (i<array.length && j<array2.length) {
+
+  let stop = 0;
+  while (i < array.length && j < array2.length) {
+    console.log("here is stop", stop);
+    console.log("here is multiSet", multiSet);
     if (array[i] == array2[j]) {
       multiSet.push(array[i]);
       multiSet.push(array2[j]);
@@ -109,15 +159,29 @@ function merge(array, array2) {
       j++;
       
     }
-    //whoever is longer becomes focus
-   //compare them 
-   //push min to front 
+    stop++
+    if (stop == 20) {
+      console.log("got infinite");
+      break
+    }
   }
+  console.log("i before while", i);
   while (i < array.length) {
-    multiSet.push(array[i++]);
+    multiSet.push(array[i]);
+    i++
+    stop++
+
+    console.log("here", i)
+    console.log("array value", array[i]);
+    console.log("i while here is stop", stop);
+    console.log("here is multiSet", multiSet);
   }
   while (j < array2.length) {
     multiSet.push(array[j++]);
+    stop++
+    console.log("array value", array[i]);
+    console.log(" j while here is stop", stop);
+    console.log("here is multiSet", multiSet);
   }
   return multiSet
 }
@@ -134,7 +198,7 @@ function mergeSort(array) {
   let result2 = mergeSort(second);
   console.log("this is result1", result1);
   console.log("this is result2", result2);
-  return merge(result1, result2);
+  return merge2point0(result1, result2);
 }
 
-console.log("this is the result of merge",merge([1,2,4,5],[1,2,3,4]));
+console.log("this is the result of merge",mergeSort([]));
