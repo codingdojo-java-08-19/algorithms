897eda02a9a5297f4bc763fc0f541922409c2e0e
Jack Tracey <jackt12390@hotmail.com>
2019-08-22 09:02:01 -0700

    algo update

--- recursion/algos.js
+++ recursion/algos.js
@@ -31,6 +31,53 @@ function factorial(num) {
     return factorial(num - 1) * Math.floor(num);
 }
 
-console.log(factorial(6.5));
+//console.log(factorial(6.5));
+
+function floodFill(canvas2D, startXY, newColor, oldColor = null) {
+    //Only replace values equal to startXY value
+    //only replace adjacent positions diagonal is not adjacent
+    // Need to look up down left and right
+    //store original color
+
+    var startX = startXY[0];
+    var startY = startXY[1];
+    if (oldColor == null) {
+        oldColor = canvas2D[startX][startY];
+        console.log(oldColor);
+    }
+    var currentColor = canvas2D[startX][startY];
+    if (currentColor != oldColor) {
+        return;
+    }
+
+    canvas2D[startX][startY] = newColor;
+
+    console.log(startX + 1);
+    if (startX + 1 < canvas2D.length) {
+        floodFill(canvas2D, [startX + 1, startY], newColor, oldColor);
+    }
+    if (startX - 1 >= 0) {
+        floodFill(canvas2D, [startX - 1, startY], newColor, oldColor);
+    }
+    if (startY + 1 < canvas2D[startX].length) {
+        floodFill(canvas2D, [startX, startY + 1], newColor, oldColor);
+    }
+    if (startY - 1 >= 0) {
+        floodFill(canvas2D, [startX, startY - 1], newColor, oldColor);
+    }
+
+    return canvas2D;
 
 
+}
+var canvas = [
+    [3, 2, 3, 4, 3],
+    [2, 3, 3, 4, 0],
+    [7, 3, 3, 5, 3],
+    [6, 5, 3, 4, 1],
+    [1, 2, 3, 3, 3],
+];
+startXY = [2, 2];
+newColor = 9;
+
+console.log(floodFill(canvas, startXY, newColor));
